f89c173e78659d611d62abe2ca73cb49eb105629
ThiagoPolli <thiagopolli.palma@gmail.com>
2020-01-08 22:36:00 -0300

    Endpoint to return a user's posts

--- src/main/java/com/thiagopolli/workshopmongo/resources/UserReasource.java
+++ src/main/java/com/thiagopolli/workshopmongo/resources/UserReasource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.thiagopolli.workshopmongo.domain.Post;
 import com.thiagopolli.workshopmongo.domain.User;
 import com.thiagopolli.workshopmongo.dto.UserDTO;
 import com.thiagopolli.workshopmongo.services.UserService;
@@ -72,6 +73,14 @@ public class UserReasource {
 		
 		return ResponseEntity.noContent().build();
 		
+	}
+	//Recurso para retornar os posts de um usuario
+	@RequestMapping(value="/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity <List<Post>> findPosts(@PathVariable String id){
+		
+		User obj = service.findById(id);
+				
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
 	
